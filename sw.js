cf7e0bad2f34a37ac5e5f65c17f9766aa14237e5
Ccasimiro <ccasimirod.18@gmail.com>
2019-02-18 00:38:45 +0100

    Modificaciones iphone x+

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ importScripts('js/sw-utils.js');
 
 
 //Constantes
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 const DYNAMIC_CACHE_LIMIT = 50;
 
@@ -55,6 +55,9 @@ self.addEventListener('activate', e =>{
         keys.forEach(key =>{
 
             if(key !== STATIC_CACHE && key.includes('static'))
+            return caches.delete(key);
+
+            if(key !== DYNAMIC_CACHE && key.includes('dynamic'))
             return caches.delete(key);
         });
     });
